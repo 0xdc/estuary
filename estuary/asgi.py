5437a5b90f1f01357e2f1070694d691a2513e8d9
Daniel Cordero <estuary@0xdc.io>
2020-08-24 12:15:37 +0000

    Enable CORS middleware

--- estuary/asgi.py
+++ estuary/asgi.py
@@ -1,5 +1,7 @@
 from starlette.applications import Starlette
 from starlette.staticfiles import StaticFiles
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
 from . import settings
 from .url import routes
@@ -9,4 +11,15 @@ if settings.DEBUG:
     static = StaticFiles(directory="static/dist", html=True)
     routes.append(Mount("/", app=static, name="static"))
 
-app = Starlette(debug=settings.DEBUG, routes=routes)
+middleware = [
+    Middleware(
+        CORSMiddleware,
+            allow_origins=['*'],
+            allow_methods=['"']
+            ),
+]
+
+app = Starlette(
+    debug=settings.DEBUG,
+    middleware=middleware,
+    routes=routes)
