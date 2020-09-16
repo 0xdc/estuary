b310551a0e337579a635775d75cb1ef361e4cbb1
Daniel Cordero <estuary@0xdc.io>
2020-09-16 08:43:57 +0000

    Consolidate database urls
    Make the database pretty much zero-config by using a default sqlite path
    
    Set up tests to use in-memory sqlite
    
    Signed-off-by: Daniel Cordero <estuary@0xdc.io>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,26 +1,21 @@
 import pytest
 from alembic import command
 from alembic.config import Config
+from starlette.config import environ
 from starlette.testclient import TestClient
-from sqlalchemy_utils import database_exists, create_database, drop_database
 
-from estuary import settings
+environ["DATABASE_URL"] = "sqlite:///:memory:"
+
+from estuary import settings # noqa
 
 
 @pytest.fixture(autouse=True)
 def create_test_database():
-    test_url = str(settings.TEST_DATABASE_URL)
-    assert not database_exists(test_url), \
-        'Test database already exists. Aborting tests.'
-    create_database(test_url)
     config = Config("alembic.ini")
-    try:
-        command.upgrade(config, "head", "--sql")  # offline, for coverage
-        command.upgrade(config, "head")
-        yield
-        command.downgrade(config, "base")
-    finally:
-        drop_database(test_url)
+    command.upgrade(config, "head", "--sql")  # offline, for coverage
+    command.upgrade(config, "head")
+    yield
+    command.downgrade(config, "base")
 
 
 @pytest.fixture()
